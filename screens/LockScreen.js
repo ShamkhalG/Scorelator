032ea014a906ed0c05b6e5bdcfb2dc8ec445e794
ShamkhalG <shamkhalguliyev83@gmail.com>
2023-08-15 16:39:34 +0400

    Added pin lock (not completed)

--- screens/LockScreen.js
+++ screens/LockScreen.js
@@ -1,12 +1,37 @@
+import { useEffect, useState, useRef } from 'react';
 import { Text, View } from 'react-native';
+import Icon from "react-native-vector-icons/Ionicons"
+import PinView from 'react-native-pin-view';
 import { lockCSS } from '../styles/lockstyle';
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
 
 export default function LockScreen() {
   const [firstLog, setFirstLog] = useState(true); // TODO: Create a database, where this variable will be stored
-  const [pin, setPin] = useState("");
+  const [correctPin, setCorrectPin] = useState("2003"); // TODO: This pin must be set from a database
+  const [correctPinText, setCorrectPinText] = useState(false);
+  const [falsePinText, setFalsePinText] = useState(false);
+  const [showRemoveButton, setShowRemoveButton] = useState(false);
+  const [enteredPin, setEnteredPin] = useState("");
+  const pinView = useRef(null);
   
+  useEffect(() => {
+    if (enteredPin.length > 0) {
+      setShowRemoveButton(true)
+    } else {
+      setShowRemoveButton(false)
+    }
+
+    if (enteredPin.length === 4) { //? TODO: If firstLog is true, then it should set the new pin
+      if (enteredPin === correctPin) {
+        setFalsePinText(false);
+        setCorrectPinText(true); // TODO: It should jump to the home page with all subjects
+      } else {
+        setCorrectPinText(false);
+        setFalsePinText(true); //! FIXME: It should remove all circles and reset the pin
+      }
+    }
+  }, [enteredPin])
+
   return (
     <View style = {lockCSS.container}>
       <StatusBar backgroundColor = '#E5E5E5' />
@@ -17,7 +42,21 @@ export default function LockScreen() {
           ( <Text style = {lockCSS.pinText}> Enter the pin </Text> )
         }
 
+        {correctPinText ? (<Text style = {lockCSS.pinText}> Correct Pin entered </Text>) : undefined}
+        {falsePinText ? (<Text style = {lockCSS.pinText}> Incorrect Pin entered </Text>) : undefined}
+
         {/* //TODO: Implement pin code lock */}
+        <PinView 
+          pinLength={4}
+          ref = {pinView}
+          onValueChange = {(value) => setEnteredPin(value)}
+          onButtonPress = {key => {
+            if (key === "custom_right"){
+              pinView.current.clear()
+            }
+          }}
+          customRightButton = {showRemoveButton ? <Icon name = {"ios-backspace"} size = {36} color = {"#FFF"} /> : undefined}
+        />
       </View>
     </View>
   );
